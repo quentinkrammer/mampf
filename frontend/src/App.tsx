3f456d577f7de7f5883c42ca67b577143c4ab649
quentinkrammer <bopatzable@gmail.com>
2024-06-02 16:06:10 +0200

    feat: Add React Router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,9 @@
-import { Button } from "@mui/material";
+import { Outlet } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <Button variant="contained">Contained</Button>
+      {"NavBar"} <Outlet />
     </>
   );
 }
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,11 +4,25 @@ import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import App from "./App";
 import "./index.css";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "myOrder",
+        element: "myOrder",
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />;
   </React.StrictMode>
 );
