6231eae8485aee3a227dd98c702fa32a366f8c41
quentinkrammer <bopatzable@gmail.com>
2024-06-02 15:43:41 +0200

    feat: Install MUI

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,38 +1,11 @@
-import { css } from "goober";
-import { PropsWithChildren } from "react";
-import "./App.css";
-import { Input } from "./components/Input";
+import { Button } from "@mui/material";
 
 function App() {
   return (
     <>
-      <Card label="Header">
-        <div>
-          <Input label="Username" />
-        </div>
-      </Card>
+      <Button variant="contained">Contained</Button>
     </>
   );
 }
 
 export default App;
-
-type CardProps = PropsWithChildren<{
-  className?: string;
-  label: string;
-}>;
-function Card({ label, children, ...otherProps }: CardProps) {
-  return (
-    <div {...otherProps}>
-      <div className={styles.header}>{label}</div>
-      {children}
-    </div>
-  );
-}
-
-const styles = {
-  header: css({
-    backgroundColor: "var(--primary-color)",
-    color: "var(--text-color-light)",
-  }),
-};
