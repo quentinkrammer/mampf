e71737b73ac2ddeeb6af8f985e59830c93b4a33a
quentinkrammer <bopatzable@gmail.com>
2024-06-02 11:01:02 +0200

    feat: Remove nest example code

--- backend/src/module/app.module.ts
+++ backend/src/module/app.module.ts
@@ -1,8 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from '../controller/app.controller';
 import { OrderController } from '../controller/order.controller';
 import { UserController } from '../controller/user.controller';
-import { AppService } from '../service/app.service';
+
 import { OrderService } from '../service/order.service';
 import { UserService } from '../service/user.service';
 import { AuthModule } from './auth.module';
@@ -10,7 +9,7 @@ import { UsersModule } from './users.module';
 
 @Module({
   imports: [AuthModule, UsersModule],
-  controllers: [AppController, UserController, OrderController],
-  providers: [AppService, UserService, OrderService],
+  controllers: [UserController, OrderController],
+  providers: [UserService, OrderService],
 })
 export class AppModule {}
