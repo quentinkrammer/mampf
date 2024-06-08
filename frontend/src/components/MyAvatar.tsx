85682f4a563266bf9bae79e8c981046c95f0dac8
quentinkrammer <bopatzable@gmail.com>
2024-06-08 16:08:08 +0200

    aff status tooltip to avatar

--- frontend/src/components/MyAvatar.tsx
+++ frontend/src/components/MyAvatar.tsx
@@ -1,4 +1,4 @@
-import { Avatar } from "@mui/material";
+import { Avatar, Tooltip } from "@mui/material";
 import { useMyUserData } from "../hooks/useMyUserData";
 import { useMyOrder } from "../hooks/useMyOrder";
 import { css, keyframes } from "goober";
@@ -7,23 +7,36 @@ export function MyAvatar() {
     const { data: user } = useMyUserData();
     const { data: orders } = useMyOrder();
 
-    return <div>
+    return <Tooltip title={<MyStatus />} placement="left">
         <Avatar key={`${user?.role}${orders?.length}`}
-            className={BLINK}>{user && user?.name.charAt(0)}</Avatar>
-    </div>;
+            className={styles.blink}>{user && user?.name.charAt(0)}</Avatar>
+    </Tooltip >;
 }
 
+
+function MyStatus() {
+    const { data: user } = useMyUserData();
+    const { data: orders } = useMyOrder();
+
+    return <div style={{ display: 'grid', gridTemplateColumns: 'auto auto', justifyItems: 'left' }}>
+        <p className={styles.tooltipLabel}>orders:</p><p>{orders?.length ?? 0}</p>
+        <p className={styles.tooltipLabel}>role: </p><p>{user?.role ?? 'none'}</p>
+    </div>
+}
 
 const BLINK_KEYFRAME = keyframes({
     '100%': {
         backgroundColor: 'greenyellow'
     }
 })
-const BLINK = css({
-    '&': {
-        animationName: BLINK_KEYFRAME,
-        animationIterationCount: 1,
-        animationDuration: '0.5s',
-        animationTimingFunction: 'ease-out'
-    }
-})
+const styles = {
+    blink: css({
+        '&': {
+            animationName: BLINK_KEYFRAME,
+            animationIterationCount: 1,
+            animationDuration: '0.5s',
+            animationTimingFunction: 'ease-out'
+        }
+    }),
+    tooltipLabel: css({ paddingRight: '0.5rem' })
+}
